4767127c7fa7e15bfcbc32ef26fa6b46705d4a08
timmoh-king <timothykariuki43@gmail.com>
2022-06-15 09:36:54 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+/**
+ * @Author: Your name
+ * @Date:   2022-06-15 09:23:06
+ * @Last Modified by:   Your name
+ * @Last Modified time: 2022-06-15 09:34:42
+ */
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <h1 class="text-3xl font-bold underline">
+    Let's start the translation here right now!
+  </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/**
+ * @Author: Your name
+ * @Date:   2022-06-15 09:27:40
+ * @Last Modified by:   Your name
+ * @Last Modified time: 2022-06-15 09:28:21
+ */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js, jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
