bfe0a29248955831592952a4056eb100f38556f1
timmoh-king <timothykariuki43@gmail.com>
2022-06-17 09:41:47 +0300

    component changes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,42 @@
  * @Author: Your name
  * @Date:   2022-06-15 09:23:06
  * @Last Modified by:   Your name
- * @Last Modified time: 2022-06-15 12:02:20
+ * @Last Modified time: 2022-06-17 09:40:33
  */
-
+import {useState} from 'react';
 import './App.css';
-import Welcome from './components/Welcome';
-import Button from './components/Button';
+import './index.css';
 
 function App() {
+  const[count, setCount] = useState(4)
+  const[text, setText] = useState("I am a Full Stack Software Developer")
+  
+  function decrement(){
+    setCount(count -1);
+  }
+  function increment(){
+    setCount (count +1)
+  }
+  function changeText(){
+    setText ("I will get a job in Jesus name")
+  }
+  // function refactorText (){
+  //   setText (useState(text))
+  // }
+ 
+  
   return (
     <div className="App">
-      <Welcome name = "Timothy" heroname = "Timmoh"> 
-      <p>This is amazing and motivating. I see potential and greatness</p></ Welcome>
-      <Welcome name = "Munene" heroname = "Nesh">
-      <p>I am a full stack web developer</p></Welcome>
-      <Welcome name = "Kariuki" heroname = "Karis" />
-      <Button />
+      
+      <button onClick ={decrement}>-</button>
+      <span>{count}</span>
+      <button onClick ={increment} >+</button>
+      
+      <h1>{text}</h1>
+      <div>
+       <button onClick={changeText} className ="Button">Affirm</button>
+       {/* <button onClick={refactorText} className='Button'>Refactor</button> */}
+      </div>
     </div>
   );
 }
