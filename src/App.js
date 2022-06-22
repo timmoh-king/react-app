e6027427898263ae8ffbb1d7ee3f7d144d1fa3a7
timmoh-king <timothykariuki43@gmail.com>
2022-06-22 23:01:59 +0300

    state changes

--- src/App.js
+++ src/App.js
@@ -2,25 +2,34 @@
  * @Author: Your name
  * @Date:   2022-06-15 09:23:06
  * @Last Modified by:   Your name
- * @Last Modified time: 2022-06-19 17:58:19
+ * @Last Modified time: 2022-06-22 21:27:28
  */
-import './App.css';
+ import "./App.css";
 import UserDashbordNavbar from "./components/common/UserDashbordNavbar";
 import Card from "./components/common/Card";
 import Homepage from "./components/common/Homepage";
-import Subscribe from './components/common/Subscribe';
+import Subscribe from "./components/common/Subscribe";
 
 function App() {
- 
+
   return (
     <div className="App">
       <UserDashbordNavbar />
       <Homepage />
       <Subscribe />
       <div className="container d-flex inline-flex py-4">
-        <Card link="https://media.istockphoto.com/vectors/young-male-character-writing-code-on-a-desktop-computer-working-from-vector-id1219473617?k=20&m=1219473617&s=612x612&w=0&h=A8e5lzJhIqWCzzY9oK0QplPYDICDnfApKpSkcBEzEhE=" name = "Software Developer"/>
-        <Card link="https://previews.123rf.com/images/ntlstudio/ntlstudio1909/ntlstudio190900406/130447137-programmer-coder-flat-vector-illustration-software-engineer-professional-developer-isolated-cartoon-.jpg" name="Data Science" />
-        <Card link="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/96701e99535677.5ef4c71eb11ca.jpg" name="Software Engineer" />
+        <Card
+          link="https://media.istockphoto.com/vectors/young-male-character-writing-code-on-a-desktop-computer-working-from-vector-id1219473617?k=20&m=1219473617&s=612x612&w=0&h=A8e5lzJhIqWCzzY9oK0QplPYDICDnfApKpSkcBEzEhE="
+          name="Software Developer"
+        />
+        <Card
+          link="https://previews.123rf.com/images/ntlstudio/ntlstudio1909/ntlstudio190900406/130447137-programmer-coder-flat-vector-illustration-software-engineer-professional-developer-isolated-cartoon-.jpg"
+          name="Data Science"
+        />
+        <Card
+          link="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/96701e99535677.5ef4c71eb11ca.jpg"
+          name="Software Engineer"
+        />
       </div>
     </div>
   );
--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -2,13 +2,16 @@
  * @Author: Your name
  * @Date:   2022-06-18 18:23:47
  * @Last Modified by:   Your name
- * @Last Modified time: 2022-06-20 09:36:30
+ * @Last Modified time: 2022-06-22 22:57:00
  */
-import React from "react"
+import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart, faComment, faShareSquare } from '@fortawesome/free-solid-svg-icons'
 
 const Card = ( props ) => {
+  const [like, setLike] = useState(0);
+  const [comment, setComment] = useState(10);
+  
   return (
     <div>
         <div className=" max-w-md rounded overflow-hidden shadow-lg p-6 mt-6">
@@ -19,22 +22,22 @@ const Card = ( props ) => {
           </div>
           <ul className="font-medium text-start mt-3">
             <li>
-              <strong>Views: </strong>
-              4000
+              <strong>Likes: </strong>
+              {like}
             </li>
             <li>
-              <strong>Downloads: </strong>
-              300
+              <strong>Comments: </strong>
+              {comment}
             </li>
             <li>
-              <strong>Views: </strong>
+              <strong>Share: </strong>
               800
             </li>
           </ul>
         </div>
-        <div className="px-6 py-1 text-start space-x-3">
-          <FontAwesomeIcon icon={faHeart} className="px-0" />
-          <FontAwesomeIcon icon={faComment} className="px-0" />
+        <div className="px-6 py-1 text-start space-x-3 ">
+          <FontAwesomeIcon icon={faHeart} className="px-0" onClick={()=> setLike(like+1)} />
+          <FontAwesomeIcon icon={faComment} className="px-0" onClick={()=> setComment(comment+1)}/>
           <FontAwesomeIcon icon={faShareSquare} className="px-0" />
         </div>
       </div>
